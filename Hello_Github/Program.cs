7e18767cd2c8f9c844c2f6bfad4097a6e8025844
Michaela Löffelmann <michaela.loffelmann@educhem.cz>
2023-03-06 10:42:34 +0100

    Přidejte soubory projektu.

---
+++ Hello_Github/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Ahoj, já jsem Michaela Löffelmannová");
+Console.WriteLine("chodím na SŠ Educhem");
+Console.WriteLine("mám rád/ baví mě:");
+
+string[] zajmy = { "programování, grafika, spát, Netflix" };
+foreach (var item in zajmy)
+{
+    Console.WriteLine(item);
+}
